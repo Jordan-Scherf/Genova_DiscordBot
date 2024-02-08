778995c4dc22c82877bc676d73ae1ceb328655b9
Jordan-Scherf <scherf.jordan@yahoo.com>
2024-02-07 22:36:30 -0800

    Broke the Spotfiy API search as it doesnt work very good

--- Genova.py
+++ Genova.py
@@ -1,3 +1,4 @@
+# These are all the modules we will be using! Continue to add to them as we expand the Bot!
 import asyncio
 import discord
 import json
@@ -7,7 +8,7 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
-#Let's grab the .env file
+#Let's grab the .env file (If you do not have one, I suggest making one!)
 load_dotenv()
 
 #Discord Setup
@@ -15,7 +16,6 @@ intents = discord.Intents.default()
 intents.presences = True
 intents.members = True
 intents.message_content = True  # Fix: Correct attribute name
-#client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='$', intents=intents)
 #Spotify Setup
 spotifyClientID = (os.getenv('SPOTIFY_CLIENT_ID'))
@@ -23,6 +23,7 @@ spotifyClientSecret = (os.getenv('SPOTIFY_CLIENT_SECRET'))
 client_credentials_manager = SpotifyClientCredentials(client_id=spotifyClientID, client_secret=spotifyClientSecret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+####################This is for the OpenAI API, which isnt currently in use###############################
 #Cannot use until money is payed :(
 # @bot.command()
 # async def test(ctx, arg):
@@ -34,29 +35,56 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 #         ]
 #     )
 #     await ctx.send(completion.choices[0].message)
+##########################################################################################################
 
+#This is a bot command, give it a try in discord! Type "$top5 "Drake"" - make sure the artist is in q
 @bot.command(name='top5')
 async def top5(ctx, arg):
-    print("In command" + arg)
-    result = sp.search(q=f'artists: {arg}', type='artist', limit=50, market='US')
-    print(result['artists']['items'])
-    for artist in result['artists']['items'][:50]:
-        if(artist['name'] == arg):
-            #Testing print
-            #print('{:<20} {:>10}'.format(artist['name'], artist['id']))
-            artist_name = artist['name']
-            artist_id = artist['id']
+    offsetNumber = 0
+    while True:
+        arg = arg.upper()
+        print("In command" + arg)
+        result = sp.search(q=f'artists: {arg}', type='artist', limit=50, offset= offsetNumber, market=None)
+        if result['artists']['total'] == 0:
+            arg = arg.lower()
+            result = sp.search(q=f'artists: {arg}', type='artist', limit=50, offset= offsetNumber , market=None)
+
+        # Check if any artists are found
+        if not result['artists']['items']:
+            #await ctx.send(f"No artists found for '{arg}'.")
+            return
+
+        # Find the matching artist
+        artist_id = None
+        artist_name = None
+        for artist in result:
+            print(result[artist])
+        for artist in result['artists']['items']:
+            print('{:<20} {:>10}'.format(artist['name'], artist['id'], int(artist['popularity'])))
+            if artist['name'].lower() == arg and int(artist['popularity']) > 40:
+                artist_name = artist['name']
+                artist_id = artist['id']
+                break
+            elif artist['name'].upper() == arg and int(artist['popularity']) > 40: 
+                artist_name = artist['name']
+                artist_id = artist['id']
+                break
+        # Check if a matching artist was found
+        if artist_id is None:
+           # await ctx.send(f"No artist found with the name '{arg}'.")
+            offsetNumber = offsetNumber + 1
         else:
-            await ctx.send("Artist Not Found")
-    top_tracks = sp.artist_top_tracks(artist_id, country='US')
+            # Retrieve top tracks for the artist
+            top_tracks = sp.artist_top_tracks(artist_id, country='US')
+            if not top_tracks['tracks']:
+                await ctx.send("No top tracks found for the artist.")
+            else:# Send the top 5 tracks to the Discord channel
+                await ctx.send(f"Top 5 songs for the artist {artist_name}:")
+                for i, track in enumerate(top_tracks['tracks'][:5]):
+                    await ctx.send(f"{i + 1}. {track['name']} - {', '.join(artist['name'] for artist in track['artists'])}")
+                return
+        
 
-    if not top_tracks['tracks']:
-        await ctx.send("No top tracks found for the artist.")
-        return
-
-    await ctx.send(f"Top 5 songs for the artist {artist_name}):")
-    for i, track in enumerate(top_tracks['tracks'][:5]):
-        await ctx.send(f"{i + 1}. {track['name']} - {', '.join(artist['name'] for artist in track['artists'])}")
 
 
 @bot.event
